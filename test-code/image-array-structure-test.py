cfa6ae0a2e55c50ec2e88ebdc137c7a8cb279047
Tom Schwarz <tomiam8@outlook.com>
2019-05-20 13:11:36 +1000

    Finish polynomial fitting test code

--- test-code/image-array-structure-test.py
+++ test-code/image-array-structure-test.py
@@ -22,13 +22,28 @@ for i in range(len(first_col)):
         if y[1]==first_col[i]:
             y_values[i].append(y[0])'''
 
-coefficients = np.polynomial.polynomial.Polynomial.fit(x_values, y_values, 1)
+coefficients = np.polyfit(y_values, x_values, 2)
 print(x_values)
 print(y_values)
 print(coefficients)
 plt.imshow(hsv_img)
-print([x for x in coefficients])
-plt.plot([x for x in range(1, len(hsv_img))], [sum([list(coefficients)[i]*(x**i) for i in range(len(coefficients))]) for x in range(1, len(hsv_img))])
+
+#Plot a bunch of values to see the polyfitting
+x_values = []
+y_values = []
+step = 10
+lower = 0
+upper = len(img)
+for x in [p/step for p in range(lower*step, upper*step+1, 1)]:
+    x_values.append(x)
+    temp_sum = 0
+    for i in range(len(coefficients)):
+        temp_sum += list(coefficients)[i]*(x**(len(coefficients)-i-1))
+    y_values.append(temp_sum)
+#print([x for x in coefficients])
+#plt.plot([x for x in range(1, len(hsv_img))], [sum([list(coefficients)[i]*(x**i) for i in range(len(coefficients))]) for x in range(1, len(hsv_img))])
+plt.imshow(img)
+plt.plot(y_values, x_values)
 plt.show()
 #cv2.imshow('image',img)
 #cv2.waitKey(0)
