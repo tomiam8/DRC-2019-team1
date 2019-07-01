f9ffe97f8ebd01689e57e73726f50469de9c839e
Baby <drc2019@notarealemailaddress.com>
2019-07-01 14:05:37 +1000

    I wanna use atom

--- andre_vision.py
+++ andre_vision.py
@@ -9,11 +9,11 @@ import time
 import serial
 import traceback #TODO remove
 
-# import pyrealsense2 as rs
-# import sys
+import pyrealsense2 as rs
+import sys
 
-# from cam import setupstream
-# from cam import getframes
+from cam import setupstream
+from cam import getframes
 
 
 _SHOW_IMAGE = True
@@ -222,9 +222,9 @@ class HandCodedLaneFollower(object):
         if found_midpoints == 0: #Just go off estimates
             for counter in range(3):
                 if yellow_points[counter] is not None:
-                    mid_points[counter] = yellow_points[counter] - width/2 #- yellow_goal_points[counter]
+                    mid_points[counter] = yellow_points[counter] -3 * width/4 #- yellow_goal_points[counter]
                 elif blue_points[counter] is not None:
-                    mid_points[counter] = width/2 + blue_points[counter] #- blue_goal_points[counter]
+                    mid_points[counter] = 3*width/4 + blue_points[counter] #- blue_goal_points[counter]
 
         elif found_midpoints == 1: #Use offset from the single midpoint
             #Find which one has the midpoint
@@ -278,11 +278,16 @@ class HandCodedLaneFollower(object):
         #Do steering stuff
         #final_frame = self.steer(frame, lane_lines)
         points = [x for x in ((mid_points[0], bottom_goal), (mid_points[1], middle_goal), (mid_points[2], top_goal))]
-        # angle = calculate_angle(points)
-        # speed = calculate_speed(points, angle)
-        #
-        # self.arduino.update_angle(angle)
-        # self.arduino.update_speed(speed)
+        angle = calculate_angle(points)
+        speed = 0
+        print(angle)
+
+        if angle:
+            speed = calculate_speed(points, angle)
+            self.arduino.update_angle(angle)
+
+        if speed:
+            self.arduino.update_speed(speed)
 
 
 
@@ -299,54 +304,64 @@ def calculate_angle(midpoints):
 
     # one midpoint == only proportional
     if len(midpoints) == 1:
-        x, y = midpoints[0]
-        # theta = x from centre to midpoint / y from centre to midpoint
-        theta = (x - width/2) / (height - y)
-        theta += 90
+        try:
+            x, y = midpoints[0]
+            # theta = x from centre to midpoint / y from centre to midpoint
+            theta = (x - width/2) / (height - y)
+            theta += 90
 
-        # -20 acts as a stabiliser
-        return kp * (theta - 20)
+            # -20 acts as a stabiliser
+            return kp * (theta - 20)
+        except:
+            return None
 
     # more than one midpoint == proportional and differential
-    if len(midpoints) == 2:
-        x1, y1 = midpoints[0]
-        x2, y2 = midpoints[1]
+    if len(midpoint_list) == 2:
+        try:
+            x1, y1 = midpoints[0]
+            x2, y2 = midpoints[1]
 
-        theta1 = (x1 - width/2) / (height - y1)
-        theta1 += 90
-        theta2 = (x2 - width/2) / (height - y2)
-        theta2 += 90
+            theta1 = (x1 - width/2) / (height - y1)
+            theta1 += 90
+            theta2 = (x2 - width/2) / (height - y2)
+            theta2 += 90
 
-        change_in_theta = (theta2 - theta1) / (y1 - y2)
+            change_in_theta = (theta2 - theta1) / (y1 - y2)
 
-        return kp * (theta1 - 20) + kd * (change_in_theta - 20 / height)
+            return kp * (theta1 - 20) + kd * (change_in_theta - 20 / height)
+        except:
+            return None
 
-    if len(midpoints) == 3:
-        x1, y1 = midpoints[0]
-        x2, y2 = midpoints[1]
-        x3, y3 = midpoints[2]
+    if len(midpoint_list) == 3:
+        try:
+            x1, y1 = midpoints[0]
+            x2, y2 = midpoints[1]
+            x3, y3 = midpoints[2]
 
-        theta1 = (x1 - width/2) / (height - y1)
-        theta1 += 90
-        theta2 = (x2 - width/2) / (height - y2)
-        theta2 += 90
-        theta3 = (x3 - width/2) / (height - y3)
-        theta3 += 90
+            theta1 = (x1 - width/2) / (height - y1)
+            theta1 += 90
+            theta2 = (x2 - width/2) / (height - y2)
+            theta2 += 90
+            theta3 = (x3 - width/2) / (height - y3)
+            theta3 += 90
 
-        change_in_theta1 = (theta2 - theta1) / (y1 - y2)
-        change_in_theta2 = (theta3 - theta2) / (y2 - y3)
-        change_in_theta_average = (change_in_theta1 + change_in_theta2) / 2
+            change_in_theta1 = (theta2 - theta1) / (y1 - y2)
+            change_in_theta2 = (theta3 - theta2) / (y2 - y3)
+            change_in_theta_average = (change_in_theta1 + change_in_theta2) / 2
 
-        # return kp * (theta1 - 20) + kd * (change_in_theta1 - 20 / height) + kd * (change_in_theta2 - 20 / height)
-        return kp * (theta1 - 20) + kd * (change_in_theta_average - 20 / height)
+            # return kp * (theta1 - 20) + kd * (change_in_theta1 - 20 / height) + kd * (change_in_theta2 - 20 / height)
+            return kp * (theta1 - 20) + kd * (change_in_theta_average - 20 / height)
+        except:
+            return None
 
     # if something wrong tho don't steer
     return 90
 
 def calculate_speed(midpoints, steer):
 
+    midpoint_list = [x for x in midpoints if x != None]
     # if no midpoints found don't move?? reconsider this later
-    if len(midpoints) == 0:
+    if len(midpoint_list) == 0:
         return 90
 
     kp = -0.333
@@ -357,38 +372,44 @@ def calculate_speed(midpoints, steer):
 
     proportional_speed = min_speed + kp * steer
     # one midpooint == only proportional
-    if len(midpoints) == 1:
+    if len(midpoint_list) == 1:
 
         return proportional_speed
 
     # more than one midpoint == proportional and integration
 
-    if len(midpoints) == 2:
-        x1, y1 = midpoints[0]
-        x2, y2 = midpoints[1]
+    if len(midpoint_list) == 2:
+        try:
+            x1, y1 = midpoints[0]
+            x2, y2 = midpoints[1]
 
-        change_in_y = y1 - y2
+            change_in_y = y1 - y2
 
-        ki = max_speed_integration / (max_speed_proportional * change_in_y)
+            ki = max_speed_integration / (max_speed_proportional * change_in_y)
 
-        return ki * change_in_y * proportional_speed
+            return ki * change_in_y * proportional_speed
+        except:
+            return None
 
-    if len(midpoints) == 3:
-        x1, y1 = midpoints[0]
-        x2, y2 = midpoints[1]
-        x3, y3 = midpoints[2]
+    if len(midpoint_list) == 3:
+        try:
+            x1, y1 = midpoints[0]
+            x2, y2 = midpoints[1]
+            x3, y3 = midpoints[2]
 
-        change_in_y_1 = y1 - y2
-        change_in_y_2 = y3 - y2
-        change_in_y_average = (change_in_y_1 + change_in_y_2) / 2
+            change_in_y_1 = y1 - y2
+            change_in_y_2 = y3 - y2
+            change_in_y_average = (change_in_y_1 + change_in_y_2) / 2
 
-        # ki_1 = max_speed_integration / (max_speed_proportional * change_in_y1)
-        # ki_2 = max_speed_integration / (max_speed_proportional * change_in_y2)
+            # ki_1 = max_speed_integration / (max_speed_proportional * change_in_y1)
+            # ki_2 = max_speed_integration / (max_speed_proportional * change_in_y2)
 
-        ki = max_speed_integration / (max_speed_proportional * change_in_y_average)
+            ki = max_speed_integration / (max_speed_proportional * change_in_y_average)
 
-        # return ki_1 * change_in_y1 * proportional_speed + ki_2 * change_in_y2 * proportional_speed
-        return ki * change_in_y_average * proportional_speed
+            # return ki_1 * change_in_y1 * proportional_speed + ki_2 * change_in_y2 * proportional_speed
+            return ki * change_in_y_average * proportional_speed
+        except:
+            return None
 
     # if something wrong tho don't move
     return 90
@@ -913,10 +934,10 @@ class File_Inputter:
                 except ValueError:
                     pass
 
-cap=cv2.VideoCapture("test5.mp4")
-video_file='test5'
-frame_input = File_Inputter()
-_thread.start_new_thread(frame_input.next_frame_counter, tuple())
+#cap=Camera()
+#video_file='test5'
+#frame_input = File_Inputter()
+#_thread.start_new_thread(frame_input.next_frame_counter, tuple())
 lane_follower = HandCodedLaneFollower()
 print("Running...")
 
@@ -925,18 +946,18 @@ def main():
 
     LIVE = True
     file = "xyz.bag"
-    #pipe, config, profile = setupstream(LIVE, file)
+    pipe, config, profile = setupstream(LIVE, file)
 
-    while cap.isOpened():
-    #while (True):
+    #while cap.isOpened():
+    while (True):
 
-            if frame_input.frame_counter < frame_input.frame_goal:
+            '''if frame_input.frame_counter < frame_input.frame_goal:
                 _, frame = cap.read()
-                frame_input.frame_counter += 1
+                frame_input.frame_counter += 1'''
 
-            #frame, depth_frame, frameset = getframes(pipe)
+            frame, depth_frame, frameset = getframes(pipe)
             combo_image = lane_follower.follow_lane(frame)
-            time.sleep(0.04)
+            #time.sleep(0.04)
 
             if cv2.waitKey(25) & 0xff == ord('q'):
                 cv2.destroyAllWindows()
